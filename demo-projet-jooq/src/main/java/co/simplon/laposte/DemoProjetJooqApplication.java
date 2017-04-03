3b676183f9b502a438b0c894f8830817612d3ce9
Abdel <Abdel@Abdel-PC>
2017-04-03 19:19:08 +0200

    First commit

---
+++ demo-projet-jooq/src/main/java/co/simplon/laposte/DemoProjetJooqApplication.java
@@ -0,0 +1,12 @@
+package co.simplon.laposte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjetJooqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjetJooqApplication.class, args);
+	}
+}
